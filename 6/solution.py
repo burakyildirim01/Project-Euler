bc694b6448eae8f79d4b39ececb22621945c067e
burakyildirim01 <31079280+burakyildirim01@users.noreply.github.com>
2020-06-13 19:04:55 +0300

    Update solution.py

--- 6/solution.py
+++ 6/solution.py
@@ -2,3 +2,9 @@ def calculatediff(n):
     return int(((n*(n+1))/2)**2 - (n*(n+1)*(2*n+1))/6)
     
 print(calculatediff(100))
+''' alternate solution
+def calculatediff(n):
+    return sum(i for i in range(n+1))**2-sum(i**2 for i in range(n+1))
+    
+print(calculatediff(100))
+'''
